84f5d931089e835ea7b027d3e7ac16389b3f24e6
Gianluca Larentis <gianluca.larentis@ecoopera.coop>
2026-05-28 16:08:09 +0200

    versione con avvio anche al boot

--- app/src/main/java/coop/ecoopera/importarubrica/BootReceiver.kt
+++ app/src/main/java/coop/ecoopera/importarubrica/BootReceiver.kt
@@ -1,5 +1,15 @@
+package coop.ecoopera.importarubrica
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.work.*
+import java.util.concurrent.TimeUnit
+
 class BootReceiver : BroadcastReceiver() {
+
     override fun onReceive(context: Context, intent: Intent) {
+
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
 
             val constraints = Constraints.Builder()
