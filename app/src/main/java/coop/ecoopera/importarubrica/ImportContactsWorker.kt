dcd10333684f1dd7cb40b13e96709a2285fb317d
Gianluca Larentis <gianluca.larentis@ecoopera.coop>
2026-05-29 08:16:34 +0200

    versione con avvio anche al boot 8

--- app/src/main/java/coop/ecoopera/importarubrica/ImportContactsWorker.kt
+++ app/src/main/java/coop/ecoopera/importarubrica/ImportContactsWorker.kt
@@ -93,8 +93,8 @@ class ImportContactsWorker(
         // ✅ Raw contact
         ops.add(
             ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
-                .withValueNull(ContactsContract.RawContacts.ACCOUNT_TYPE,null)
-                .withValueNull(ContactsContract.RawContacts.ACCOUNT_NAME,null)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE,null)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME,null)
                 .build()
         )
 
